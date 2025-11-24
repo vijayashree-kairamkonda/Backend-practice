897940e65f65f6096a83548aac55f5803c3fae0b
vkk1000 <75258563+vijayashree-kairamkonda@users.noreply.github.com>
2025-11-24 14:27:49 +0530

    Added customer routes and urlencoded middleware

--- server4.js
+++ server4.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { config as CONFIG } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import customerRoutes from "./src/routes/customerRoutes.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 console.log(process.env);
 
 connectDB();
+
+app.use("/api/customers", customerRoutes);
 
 app.listen(CONFIG.port, () => {
   console.log(
